ee077d7c1d302ca2495e8b7db3ac7f68ea723707
Aarne Harju <aarne.harju@sogeti.com>
2024-08-27 11:31:15 +0300

    Added SignUp component and tests for it - green

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -7,4 +7,39 @@ describe("Sign up page tests", () => {
     render(SignUp);
     const headerElement = screen.getByRole("heading", { name: /Sign Up/i });
   });
+
+  it("has username input", () => {
+    render(SignUp);
+    const inputUsernameElement = screen.getByRole("textbox", { name: /username/i }); // aria-label needed in html to make the getByRole work
+    // const inputUsernameElement = screen.getByLabelText(/username/i);
+    // expect(inputUsernameElement).toBeInTheDocument(); <-- expect is not needed, as it's implicitly tested in the previous step
+  });
+
+  it("has email input", () => {
+    render(SignUp);
+    const inputEmailElement = screen.getByLabelText(/email/i);
+  });
+
+  it("has password type for password input", () => {
+    render(SignUp);
+    const inputPasswordElement = screen.getByLabelText(/^password/i);
+    expect(inputPasswordElement).toHaveAttribute("type", "password");
+  });
+
+  it("has password type for repeat password input", () => {
+    render(SignUp);
+    const inputPasswordElement = screen.getByLabelText(/repeat password/i);
+    expect(inputPasswordElement).toHaveAttribute("type", "password");
+  });
+
+  it("has a sign up button", () => {
+    render(SignUp);
+    const buttonElement = screen.getByRole("button", { name: /Sign up/i });
+  });
+
+  it("has the sign up button disabled initially", () => {
+    render(SignUp);
+    const buttonElement = screen.getByRole("button", { name: /Sign up/i });
+    expect(buttonElement).toBeDisabled();
+  });
 });
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,5 +1,24 @@
 <script></script>
 <template>
   <h1>Sign Up</h1>
+  <form>
+    <div>
+      <label for="username" hidden>Username:</label>
+      <input type="text" id="username" placeholder="Username" aria-label="username"></input>
+    </div>
+    <div>
+      <label for="email" hidden>Email:</label>
+      <input type="text" id="email" placeholder="Email"></input>
+    </div>
+    <div>
+      <label for="password" hidden>Password:</label>
+      <input type="password" id="password" placeholder="Password"></input>
+    </div>
+    <div>
+      <label for="repeat-password" hidden>Repeat password:</label>
+      <input type="password" id="repeat-password" placeholder="Repeat password"></input>
+    </div>
+    <button type="submit" disabled>Sign up</button>
+  </form>
 </template>
 <style></style>
